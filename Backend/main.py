18452b1bf67ef73c8c021731916260a836c14acf
David Pérez Zuniga <davidhumbertop503@gmail.com>
2026-01-04 15:32:02 -0600

    Feat: Usando Lifespan para controlar el servidor. Base de datos y tablas funcionales

--- Backend/main.py
+++ Backend/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from database.db import on_table_and_database
+from contextlib import asynccontextmanager
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    on_table_and_database()
+    print("Base de datos y tablas viven")
+    yield
+    print("Base de datos y tablas mueren")
 
-app = FastAPI()
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 def home():
-    return {"mensaje": "¡Hola, mi Backend está vivo!", "estado": "ok"}
+    return {"mensaje": "Servidor funcionando", "estado": "ok"}
