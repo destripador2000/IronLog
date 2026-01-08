87c10d79267234c5638f75bd030fcd7e81eebfee
David Pérez Zuniga <davidhumbertop503@gmail.com>
2026-01-07 21:03:41 -0600

    Refactor: Nuevos campos añadidos en la tabla usuarios. Tabla completa

--- Backend/models/md_Users.py
+++ Backend/models/md_Users.py
@@ -3,5 +3,14 @@ from sqlmodel import SQLModel, Field
 class UserBase(SQLModel, table=True):
     
     id: int = Field(default=None, primary_key=True)
+    names: str
+    last_names: str
+    phone: str
+    birth_date: str
+    cdi: str
+    gender: str
     username: str 
     password: str
+    email: str
+
+    
