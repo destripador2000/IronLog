1403427bc57d90c9f892d564c2863f4527f4f513
David Pérez Zuniga <davidhumbertop503@gmail.com>
2026-01-10 17:05:57 -0600

    Feat: API UserLogin consumida en el frontend, testeo existoso.

---
+++ Frontend/Actions_forms/action_login.ts
@@ -0,0 +1,39 @@
+const txt_username = document.getElementById("txt_username") as HTMLInputElement;
+const txt_password = document.getElementById("txt_password") as HTMLInputElement;
+const btn_login = document.getElementById("btn_login") as HTMLButtonElement;
+const URL = import.meta.env.VITE_URL_LOGIN_USER;
+
+interface UserData {
+    username: string;
+    password: string;
+}
+
+btn_login.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const UserJson: UserData = {
+        username: txt_username.value.trim(),
+        password: txt_password.value.trim()
+    }; 
+    try {
+        
+        const response = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(UserJson),
+            credentials: 'include'
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            alert("Inicio de sesión exitoso.");
+        } else {
+            alert(data.error || "Error en el inicio de sesión");
+        }
+    } catch (error) {
+        console.error("Error al iniciar sesión:", error);
+        alert("Error de conexión con el servidor");
+    }
+});
