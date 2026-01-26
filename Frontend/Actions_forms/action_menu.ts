c8172b040195dafa95771a3b1b89a57e6e41bed1
David Pérez Zuniga <davidhumbertop503@gmail.com>
2026-01-26 16:49:44 -0600

    Refactor: Modificando body del menú. Y funciones de boton usuario y calendario funcional

---
+++ Frontend/Actions_forms/action_menu.ts
@@ -0,0 +1,13 @@
+const btn_calendar = document.getElementById("btn_calendar") as HTMLButtonElement
+const btn_user = document.getElementById("btn_user") as HTMLButtonElement
+
+btn_calendar.addEventListener('click', (e)=>{
+    e.preventDefault();
+    window.location.replace("../Body_forms/calendar_body.html")
+})
+
+btn_user.addEventListener('click', (e)=>{
+    e.preventDefault();
+    window.location.replace("../Body_forms/user_body.html")
+
+})
